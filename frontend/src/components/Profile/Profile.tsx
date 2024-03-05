ff6dbd8c4d3605e79ecafd579d4c50ff6cd2d300
livinginatent <suleyman.eminbeyli@gmail.com>
2024-03-05 16:24:50 +0400

    profile page progress

--- frontend/src/components/Profile/Profile.tsx
+++ frontend/src/components/Profile/Profile.tsx
@@ -4,34 +4,42 @@ import React from "react";
 import { SlBadge } from "react-icons/sl";
 import { FaRegChartBar } from "react-icons/fa";
 import { BiMath } from "react-icons/bi";
+import UserInfo from "../UserInfo/UserInfo";
+import User from "@/app/models/userModel";
 type Props = {};
 
 const Profile = (props: Props) => {
   return (
     <main className="bg-[#eeeeee]">
       <section className="flex flex-col ml-8 gap-4 ">
-        <div className="rounded-md w-[14rem] h-[16rem] mt-4 bg-white flex flex-col justify-items-center items-center p-4">
-          <Avatar color="purple" size="xl" tone="glossy" />
-          <p className="mt-4">Dan Abramov</p>
-          <p className="mt-4 font-extralight text-slate-600">Fast Learner</p>
-          <p className="mt-4 font-thin text-slate-600">Baku, Azerbaijan</p>
-          <div className="flex mt-4">
-            <Button className="h-6  " tone="outline">
-              Share
-            </Button>
+        <div className="flex">
+          <div className="rounded-md w-[14rem] h-[16rem] mt-4 bg-white flex flex-col justify-items-center items-center p-4">
+            <Avatar color="purple" size="xl" tone="glossy" />
+            <p className="mt-4">Dan Abramov</p>
+            <p className="mt-4 font-extralight text-slate-600">Fast Learner</p>
+            <p className="mt-4 font-thin text-slate-600">Baku, Azerbaijan</p>
+            <div className="flex mt-4">
+              <Button className="h-6  " tone="outline">
+                Share
+              </Button>
+            </div>
           </div>
+          <UserInfo />
         </div>
-        <Card className="w-[14rem]">
-          <Card.Body className="flex h-10 flex-row gap-2 justify-items-center items-center p-2 font-light">
-            {<SlBadge />}Total Badges: 20
-          </Card.Body>
-          <Card.Body className="flex h-10 flex-row gap-2 justify-items-center items-center p-2 font-light">
-            {<FaRegChartBar />} Skill Level: Expert
-          </Card.Body>
-          <Card.Body className="flex h-10 flex-row gap-2 justify-items-center items-center p-2 font-light">
-            {<BiMath />} Favourite Challenge: Addition
-          </Card.Body>
-        </Card>
+
+        <div>
+          <Card className="w-[14rem]">
+            <Card.Body className="flex h-10 flex-row gap-2 justify-items-center items-center p-2 font-light">
+              {<SlBadge />}Total Badges: 20
+            </Card.Body>
+            <Card.Body className="flex h-10 flex-row gap-2 justify-items-center items-center p-2 font-light">
+              {<FaRegChartBar />} Skill Level: Expert
+            </Card.Body>
+            <Card.Body className="flex h-10 flex-row gap-2 justify-items-center items-center p-2 font-light">
+              {<BiMath />} Favourite Challenge: Addition
+            </Card.Body>
+          </Card>
+        </div>
       </section>
     </main>
   );
---
+++ frontend/src/components/UserProgress/UserProgress.tsx
@@ -0,0 +1,16 @@
+import { Card } from '@rewind-ui/core'
+import React from 'react'
+
+type Props = {}
+
+const UserProgress = (props: Props) => {
+  return (
+    <section>
+        <Card>
+
+        </Card>
+    </section>
+  )
+}
+
+export default UserProgress
